989abe9294267600a8fb7c49dcf075ea3281efcb
Memolia <ultimum.memolia@gmail.com>
2025-04-28 20:58:31 +0900

    Revert "セクション４記載"
    This reverts commit 7540685a913941c5c645d4ee49a1059f6969cf09.

--- src/bin/mod.rs
+++ src/bin/mod.rs
@@ -1,6 +1,5 @@
 pub mod section2;
-pub mod section3;
+
 pub fn main() {
     section2::main();
-    section3::main();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,4 @@ fn main() {
     println!("プログラムを開始します");
 
     bin::section2::main();
-    bin::section3::main();
 }
